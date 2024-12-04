b30c6a592bb4f8f1b322709c0e77c5af94e94f46
Tofutush <Tofutush@users.noreply.github.com>
2024-12-04 02:27:49 +0000

    deploy: 2bf49ed0acb3ce6ce0af7041018d201cdee24edf

---
+++ js/fakeredirect.js
@@ -0,0 +1,39 @@
+function fakeRedirect(mode) {
+    fetch('/oc-webring/_data/sites.json')
+        .then(response => response.json()).then(siteList => {
+            let url = new URLSearchParams(window.location.search);
+            let originalSite = url.get('site');
+            let index;
+            for (let z = 0; z < siteList.length; z++) {
+                if (siteList[z].name === originalSite) {
+                    index = z;
+                    break;
+                }
+            }
+            if (index === undefined) {
+                window.location.assign('/oc-webring/no-such-site/');
+                return;
+            }
+            switch (mode) {
+                case 'prev':
+                    window.location.assign(siteList[index === 0 ? siteList.length - 1 : index - 1].ocurl);
+                    break;
+                case 'next':
+                    window.location.assign(siteList[index === siteList.length - 1 ? 0 : index + 1].ocurl);
+                    break;
+                case 'rand':
+                    window.location.assign(siteList[getRandomNumberButExcludeOne(0, siteList.length - 1, index)].ocurl);
+                    break;
+                default:
+                    throw new Error(`redirect mode ${mode} not found`);
+            }
+        }).catch(err => console.error(err));
+}
+
+function getRandomNumberButExcludeOne(min, max, exclude) {
+    let num;
+    do {
+        num = Math.floor(Math.random() * (max - min + 1)) + min;
+    } while (num === exclude);
+    return num;
+}
