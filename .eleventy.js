8134076944bed91324f27ebaabe80b2718cfa9d9
Tofutush <siroccoxxq@gmail.com>
2024-12-05 10:33:28 +0800

    fonts

--- .eleventy.js
+++ .eleventy.js
@@ -2,5 +2,7 @@ module.exports = async function (eleventyConfig) {
 	const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPassthroughCopy('_data');
+	eleventyConfig.addPassthroughCopy('css');
 	eleventyConfig.addPassthroughCopy('js');
+	eleventyConfig.addPassthroughCopy('fonts');
 }
---
+++ credit.md
@@ -0,0 +1,1 @@
+- 中文字体用的是[霞鹜文楷 v1.501](https://github.com/lxgw/LxgwWenKai)。
