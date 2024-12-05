53c2fd97f4789ede6264adf0c0207b99895555b3
Tofutush <siroccoxxq@gmail.com>
2024-12-05 20:14:30 +0800

    sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
 module.exports = async function (eleventyConfig) {
 	const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
+	const { minify } = await import('html-minifier-terser');
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPassthroughCopy('_data');
 	eleventyConfig.addPassthroughCopy('css');
 	eleventyConfig.addPassthroughCopy('js');
 	eleventyConfig.addPassthroughCopy('fonts');
+	eleventyConfig.addTransform("htmlmin", async function (content) {
+		if ((this.page.outputPath || "").endsWith(".html")) {
+			let minified = await minify(content, {
+				collapseWhitespace: true,
+				minifyCSS: true,
+				minifyJS: true
+			});
+			return minified;
+		}
+		// If not an HTML output, return content as-is
+		return content;
+	});
 }
