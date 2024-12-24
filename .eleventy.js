88e4108424830a95717d91db9b23fab675a4d017
Tofutush <xiexuanqisirocco@163.com>
2024-12-24 14:19:08 +0800

    modules

--- .eleventy.js
+++ .eleventy.js
@@ -1,26 +1,27 @@
-module.exports = async function (eleventyConfig) {
-	const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
-	const { EleventyI18nPlugin } = await import("@11ty/eleventy");
-	const { minify } = await import('html-minifier-terser');
-	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-	eleventyConfig.addPlugin(EleventyI18nPlugin, {
-		defaultLanguage: 'en'
-	});
-	eleventyConfig.addPassthroughCopy('_data');
-	eleventyConfig.addPassthroughCopy('fonts');
-	eleventyConfig.addPassthroughCopy('img');
-	eleventyConfig.addPassthroughCopy('css');
-	eleventyConfig.addPassthroughCopy('js');
-	eleventyConfig.addTransform("htmlmin", async function (content) {
-		if ((this.page.outputPath || "").endsWith(".html")) {
-			let minified = await minify(content, {
-				collapseWhitespace: true,
-				minifyCSS: true,
-				minifyJS: true
-			});
-			return minified;
-		}
-		// If not an HTML output, return content as-is
-		return content;
-	});
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+import { EleventyI18nPlugin } from "@11ty/eleventy";
+import { minify } from 'html-minifier-terser';
+
+export default function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+        defaultLanguage: 'en'
+    });
+    eleventyConfig.addPassthroughCopy('_data');
+    eleventyConfig.addPassthroughCopy('fonts');
+    eleventyConfig.addPassthroughCopy('img');
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('js');
+    eleventyConfig.addTransform("htmlmin", async function (content) {
+        if ((this.page.outputPath || "").endsWith(".html")) {
+            let minified = await minify(content, {
+                collapseWhitespace: true,
+                minifyCSS: true,
+                minifyJS: true
+            });
+            return minified;
+        }
+        // If not an HTML output, return content as-is
+        return content;
+    });
 }
