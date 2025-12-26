30e5183077b53f74a06edcd7bcb74902573b1743
Tofutush <xiexuanqisirocco@163.com>
2025-12-26 13:45:56 -0500

    tag shit

--- _data/tagData.js
+++ _data/tagData.js
@@ -1,3 +1,11 @@
 import sites from './sites.json' with {type: 'json'};
 
-export default [...new Set(sites.flatMap(site => site.tags ?? []))].sort((a, b) => a.localeCompare(b));
+export default Object.entries(
+    sites.reduce((acc, { tags }) => {
+        if (!Array.isArray(tags)) return acc;
+        tags.forEach(t => acc[t] = (acc[t] || 0) + 1);
+        return acc;
+    }, {})
+)
+    .sort((a, b) => b[1] - a[1] || a[0].localeCompare(b[0]))
+    .map(([t]) => t);
